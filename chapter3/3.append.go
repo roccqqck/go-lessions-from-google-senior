d4b9d44db019fd97532a60777293bb5beeb6e2cf
roccqqck <roccqqck@gmail.com>
2022-08-24 14:51:44 +0800

    append

---
+++ chapter3/3.append.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	s1 := arr[2:6]
+	s2 := s1[3:5]
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+
+	fmt.Println("s1 = ", s1)
+	fmt.Println("s2 = ", s2)
+	fmt.Println("s3 = ", s3)
+	fmt.Println("s4 = ", s4)
+	fmt.Println("s5 = ", s5)
+	fmt.Println("arr = ", arr)
+	// s4 and s5 no longer view arr
+
+}
